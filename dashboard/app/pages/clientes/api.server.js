6773f7244a2318f1c70ee3461a81236bf8815be1
luanAfons0 <luan.hmatao@gmail.com>
2025-10-31 21:59:32 -0300

    update(kottster-form): add email and document validation

--- dashboard/app/pages/clientes/api.server.js
+++ dashboard/app/pages/clientes/api.server.js
@@ -1,12 +1,33 @@
 import { app } from "../../_server/app";
 
-const controller = app.defineTableController({
-  // Validate before inserting a record
-  validateRecordBeforeInsert: (values) => {
-    console.log(values);
+import dataSource from "../../_server/data-sources/mysql_db_609ei2/";
 
-    if (!values.email.includes("@")) {
+const knex = dataSource.getClient();
+
+const controller = app.defineTableController({
+  validateRecordBeforeInsert: async (values) => {
+    // Validate client email
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!regex.test(values.email)) {
       throw new Error("Invalid email");
+    }
+
+    const emailExists = await knex("client")
+      .where({ email: values.email })
+      .first();
+
+    if (emailExists) {
+      throw new Error("User with this email already exists.");
+    }
+
+    // Validate client document
+    const documentExists = await knex("client")
+      .where({ document: values.document })
+      .first();
+
+    if (documentExists) {
+      throw new Error("User with this document already exists.");
     }
 
     return true;
